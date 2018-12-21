d923e3ae020d211e065cb548771b2b97f582577d
dasitha@sp-solutions.biz <dasitha@sp-solutions.biz>
2018-12-21 09:11:38 +0530

    changed the commentrepository native querry parameter setting

--- src/main/java/edu/bit/sms/scheduler/repository/CommentRepository.java
+++ src/main/java/edu/bit/sms/scheduler/repository/CommentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +15,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     Page<Comment> findByPostId(Long postId, Pageable pageable);
 
-    @Query(value = "select * from comments where post_id= ?", nativeQuery = true)
-    List<Comment> findCommentsByPostId(Long postIdv);
+    @Query(value = "select * from comments where post_id= :postIdv", nativeQuery = true)
+    List<Comment> findCommentsByPostId(@Param("postIdv") Long postIdv);
 }
